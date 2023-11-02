53fdf9fdef477a0ca42b3d0226b02df6dcbe9cd1
Rhaetional <12344096+rhaetional@users.noreply.github.com>
2023-11-02 15:04:05 +0100

    RHAE02 Buttons 'Align and Distribute'
    + integrate change manager

--- src/main/java/AutoDriveEditor/GUI/Buttons/Alignment/VerticalAlignDistrButton.java
+++ src/main/java/AutoDriveEditor/GUI/Buttons/Alignment/VerticalAlignDistrButton.java
@@ -2,6 +2,7 @@ package AutoDriveEditor.GUI.Buttons.Alignment;
 
 import AutoDriveEditor.GUI.Buttons.AlignBaseButton;
 import AutoDriveEditor.RoadNetwork.MapNode;
+import AutoDriveEditor.Utils.Classes.CoordinateChanger;
 
 import javax.swing.*;
 
@@ -16,57 +17,71 @@ import static AutoDriveEditor.Utils.MathUtils.roundUpDoubleToDecimalPlaces;
 
 public class VerticalAlignDistrButton extends AlignBaseButton {
 
+    // debug flag (local, hard-coded for now)
+    private boolean DEBUG = false;
+
     public VerticalAlignDistrButton(JPanel panel) {
-        button = makeImageToggleButton("buttons/verticalaligndistr","buttons/verticalaligndistr_selected", null,"align_vertical_distribute_tooltip","align_vertical_distribute_alt", panel, false, false, null, false, this);
+        button = makeImageToggleButton("buttons/verticalaligndistr", "buttons/verticalaligndistr_selected", null, "align_vertical_distribute_tooltip", "align_vertical_distribute_alt", panel, false, false, null, false, this);
     }
 
     @Override
-    public String getButtonID() { return "VerticalAlignDistributeButton"; }
+    public String getButtonID() {
+        return "VerticalAlignDistributeButton";
+    }
 
     @Override
-    public String getButtonAction() { return "ActionButton"; }
+    public String getButtonAction() {
+        return "ActionButton";
+    }
 
     @Override
-    public String getButtonPanel() { return "Alignment"; }
+    public String getButtonPanel() {
+        return "Alignment";
+    }
 
     @Override
-    public String getInfoText() { return getLocaleString("align_vertical_distribution_tooltip"); }
+    public String getInfoText() {
+        return getLocaleString("align_vertical_distribution_tooltip");
+    }
 
     @Override
     protected void adjustNodesTo(MapNode toNode) {
 
-        //local debug flag
-        boolean bDebugVAlignDistr = false;
+        if (!multiSelectList.isEmpty()) {
+            LOG.info("Vertically Aligning'n'Distributing {} nodes at world X coordinate {}", multiSelectList.size(), toNode.x);
+            CoordinateChanger coordChange = new CoordinateChanger();
 
-        LOG.info("Vertically Aligning'n'Distributing {} nodes at world X coordinate {}",multiSelectList.size(), toNode.x);
-        changeManager.addChangeable( new AlignmentChanger(multiSelectList, toNode.x, 0, 0));
+            // print original coord for debugging
+            if (DEBUG)
+                for (MapNode node : multiSelectList)
+                    LOG.info("pre VAD x: {} y: {} z: {}", node.x, node.y, node.z);
 
-        // print original coord for debugging
-        if (bDebugVAlignDistr)
-            for (MapNode node : multiSelectList)
-                LOG.info("pre VAD x: {} y: {} z: {}", node.x, node.y, node.z);
+            // Sort List by Z coordinate
+            multiSelectList.sort(Comparator.comparingDouble(value -> value.z));
 
-        // Sort List by Z coordinate
-        multiSelectList.sort(Comparator.comparingDouble(value -> value.z));
+            // calculate distance
+            double distance = multiSelectList.getLast().z - multiSelectList.getFirst().z;
+            int gapCount = multiSelectList.size() - 1;
+            // calculate element step to three decimals
+            double stepSize = distance / gapCount;
+            double currentZ = multiSelectList.getFirst().z;
 
-        // calculate distance
-        double distance = multiSelectList.getLast().z - multiSelectList.getFirst().z;
-        int gapCount = multiSelectList.size()-1;
-        // calculate element step to three decimals
-        double stepSize = distance / gapCount;
-        double currentZ = multiSelectList.getFirst().z;
+            if (DEBUG)
+                LOG.info("VAD distance: {} gaps : {} step-size: {} startingZ: {}", distance, gapCount, stepSize, currentZ);
 
-        if (bDebugVAlignDistr) LOG.info("VAD distance: {} gaps : {} step-size: {} startingZ: {}", distance, gapCount, stepSize, currentZ);
+            // apply by setting z = z(min)+element_step
+            for (MapNode node : multiSelectList) {
+                currentZ = roundUpDoubleToDecimalPlaces(currentZ, 3);
+                coordChange.addCoordinateChange(node, toNode.x, node.y, currentZ);
 
-        // apply by setting z = z(min)+element_step
-        for (MapNode node : multiSelectList) {
-            node.x = toNode.x;
-            node.z = roundUpDoubleToDecimalPlaces(currentZ, 3);
-            currentZ = currentZ + stepSize;
+                node.x = toNode.x;
+                node.z = currentZ;
+                currentZ = currentZ + stepSize;
 
-            if (bDebugVAlignDistr) LOG.info("post VAD x: {} y: {} z: {}",node.x, node.y,node.z);
+                if (DEBUG) LOG.info("post VAD x: {} y: {} z: {}", node.x, node.y, node.z);
+            }
+
+            changeManager.addChangeable(coordChange);
         }
-
-
     }
 }
